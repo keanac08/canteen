acfe7b9a2a5d5f7d1023cb3c5da274ca3f21ebc5
Chris Desiderio <keanac08@gmail.com>
2018-09-10 16:04:20 +0800

    Update server.js

---
+++ canteen-server.bat
@@ -0,0 +1,6 @@
+echo off
+cd c:/
+cd wamp\www\canteen_v2
+cls
+node index.js
+pause
--- index.js
+++ index.js
@@ -15,59 +15,60 @@ nsp.on('connection', function (socket) {
 	
 	socket.on('join_session', function (client_ip) {
 		session = client_ip;
+		socket.room = client_ip;
 		socket.join(session);
 	});
 	
 	socket.on('new_cart_item', function (data) {
 		//~ console.log('Client : ' + session + ' - NewCartItem____' + data.name + '____@' +   data.price + '____x' + data.quantity + '____' + data.total);
 		//~ console.log(data);
-		nsp.to(session).emit("new_cart_item", data);
+		nsp.to(socket.room).emit("new_cart_item", data);
 	});	
 	
 	socket.on('update_cart_item', function (data) {
 		//~ console.log('Client : ' + session + ' - UpdateCartItem____' + data.name + '____@' +   data.price + '____x' + data.quantity + '____' + data.total);
 		//~ console.log(data);
-		nsp.to(session).emit("update_cart_item", data);
+		nsp.to(socket.room).emit("update_cart_item", data);
 	});	
 	
 	socket.on('delete_cart_item', function (data) {
 		//~ console.log('Client : ' + session + ' - DeleteCartItem____' + data.name);
 		//~ console.log(data);
-		nsp.to(session).emit("delete_cart_item", data);
+		nsp.to(socket.room).emit("delete_cart_item", data);
 	});	
 	
 	socket.on('update_cart_total', function (data) {
 		//~ console.log('Client : ' + session + ' - CartTotal ' + data.total );
 		//~ console.log(data);
-		nsp.to(session).emit("update_cart_total", data);
+		nsp.to(socket.room).emit("update_cart_total", data);
 	});	
 	
 	socket.on('update_balance', function (data) {
 		//~ console.log('Client : ' + session + ' - CartTotal ' + data.total );
 		//~ console.log(data);
-		nsp.to(session).emit("update_balance", data);
+		nsp.to(socket.room).emit("update_balance", data);
 	});	
 	
 	socket.on('clear_cart', function () {
 		//~ console.log('Client : ' + session + ' - ClearCart');
 		//~ console.log(data);
-		nsp.to(session).emit("clear_cart");
+		nsp.to(socket.room).emit("clear_cart");
 	});	
 	
 	socket.on('transaction_completed', function () {
 		//~ console.log(data);
-		nsp.to(session).emit("transaction_completed");
+		nsp.to(socket.room).emit("transaction_completed");
 	});	
 	
 	socket.on('employee_details', function (data) {
 		//~ console.log('Employee : ' + data.employee.number + ' - ' + data.employee.name);
 		//~ console.log(data);
-		nsp.to(session).emit("employee_details", data);
+		nsp.to(socket.room).emit("employee_details", data);
 	});	
 	
 	socket.on('refresh', function () {
 		//~ console.log('refresh');
-		nsp.to(session).emit("refresh");
+		nsp.to(socket.room).emit("refresh");
 	});	
 	
 });
